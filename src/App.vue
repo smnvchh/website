dd48bd151777fecdf64253c432d0b982f127af97
Yura <semenovich.004@gmail.com>
2022-04-05 23:11:47 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="post">
+            <div><strong>Название: </strong>Пост о JavaScript</div>
+            <div><strong>Описание: </strong>JavaScript универс яз прог</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+        }
+    },
+    methods: {
+        addLike(){
+            this.likes += 1;
+        },
+        addDislike(){
+            this.dislikes += 1;
+        }
+    }    
+    
+}
+</script>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+</style>
